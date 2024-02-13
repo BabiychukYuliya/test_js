15c584e758f477dbb3368fe4c074750d20a5c839
BabiychukYuliya <julia4233097@gmail.com>
2024-02-13 22:39:15 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,9 +1,22 @@
-const array = [67, -56, 25, 98, -103, 87, -101];
-let max = array[0];
+// const array = [67, -56, 25, 98, -103, 87, -101];
+// let max = array[0];
 
-for (let i = 1; i < array.length; i++) {
-  if (array[i] > max) {
-    max = array[i];
-  }
-}
-console.log(max);
+// for (let i = 1; i < array.length; i += 1) {
+//   if (array[i] > max) {
+//     max = array[i];
+//   }
+// }
+// console.log(max);
+
+const players = [
+  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+
+const totalAveragePlaytimePerGame = players.reduce(
+  (acc, player) => acc + player.playtime / player.gamesPlayed,
+  0
+);
+console.log(totalAveragePlaytimePerGame);
