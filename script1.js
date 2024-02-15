b25b16c975a86b16daad3836d23d9a43ec0f7370
BabiychukYuliya <julia4233097@gmail.com>
2024-02-15 14:21:58 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -8,15 +8,19 @@
 // }
 // console.log(max);
 
-const players = [
-  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
-  { name: "Poly", playtime: 469, gamesPlayed: 2 },
-  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
-  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
-];
+// const players = [
+//   { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+//   { name: "Poly", playtime: 469, gamesPlayed: 2 },
+//   { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+//   { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+// ];
 
-const totalAveragePlaytimePerGame = players.reduce(
-  (acc, player) => acc + player.playtime / player.gamesPlayed,
-  0
-);
-console.log(totalAveragePlaytimePerGame);
+// const totalAveragePlaytimePerGame = players.reduce(
+//   (acc, player) => acc + player.playtime / player.gamesPlayed,
+//   0
+// );
+// console.log(totalAveragePlaytimePerGame);
+
+const salary = [500, 650, 800, 950];
+const total = salary.reduceRight((acc, elem) => acc + elem);
+console.log(total); //2900
